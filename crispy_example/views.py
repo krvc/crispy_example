cbd9fe970dd30d3843113b8c7adb8a3dc21339d3
krvc <krvc.it@gmail.com>
2014-04-24 16:21:25 +0530

    Added requirements

--- crispy_example/views.py
+++ crispy_example/views.py
@@ -4,6 +4,5 @@ from crispy_example.forms import SimpleForm
 
 
 class Index1View(FormView):
-    # import ipdb; ipdb.set_trace()
     template_name = 'crispy_example/index.html'
     form_class = SimpleForm
